dc96e4e9ec390a77e3b1d67b1712f39c282a649b
SamiAlmouss <45295635+SamiAlmouss@users.noreply.github.com>
2026-02-15 20:01:29 +0100

    Refactor regex matches for titles and durations

--- index55.php
+++ index55.php
@@ -14,30 +14,24 @@ $durations = [];
 preg_match_all(
     '/"title"\s*:\s*\{"runs"\s*:\s*\[\{"text"\s*:\s*"([^"]+)"\}\]\s*,"accessibility"/u',
     $source,
-    $matches
+    $matches1
 );
 
-if (!empty($matches[1])) {
-    $titles = $matches[1];
+if (!empty($matches1[1])) {
+    $titles = $matches1[1];
 };
-// '/"simpleText"\s*:\s*"(\d{1,2}:\d{2}(?::\d{2})?)"/u',
+//================================================
 preg_match_all(
-  '/"label":"(\d+)\s*minutes?,\s*(\d+)\s*seconds?"/',
-  $text,
-  $durationMatches2);
-if (!empty($durationMatches2)) {
-    $durations2 = $durationMatches2[1] .':'. $durationMatches2[2];}
+    '/{"accessibilityData":{"label":"\d+ minutes,\s*\d+\s*seconds"}},"simpleText":"(\d+:\d+)"},"style":"DEFAULT"}/',
+    $source,
+    $matches2
+);
 
-
-preg_match_all(
-  '/"simpleText"\s*:\s*"(\d{1,2}:\d{2}(?::\d{2})?)"\s*},\s*"style"/u',
-  $source,
-  $durationMatches);
-
-
-if (!empty($durationMatches[1])) {
-    $durations = $durationMatches[1];
+if (!empty($matches2[1])) {
+    $durations = $matches2[1];
 };
+
+//=========================================================
 
 function getMatches($string, $pattern) {
     preg_match_all($pattern, $string, $matches);
@@ -67,7 +61,6 @@ $response = [
     "URLs" => $URLs,
     "titles" => $titles,
     "durations" => $durations,
-    "durations2" => $durations2,
     ];
 
 echo json_encode($response,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
